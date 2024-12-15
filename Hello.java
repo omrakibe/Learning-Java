f2e0859c3ba76f22747869eb37d98588c251b55e
Om Rakibe <rakibeom30@gmail.com>
2024-12-15 09:58:34 +0530

    Adding file to new repo

---
+++ Hello.java
@@ -0,0 +1,11 @@
+class Hello
+{
+    public static void main(String a[])
+        {
+            int num1 = 2;
+            int num2 = 7;
+            int result = num1 + num2;
+            System.out.println("Hello World");
+            System.out.println(result);
+        }
+}
