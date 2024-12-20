1eaad4716a41de30e4eacda1dbd1f866991511db
Om Rakibe <rakibeom30@gmail.com>
2024-12-20 15:12:25 +0530

    Learned about arithematic operations and relational operators

---
+++ 5. Arithematic Operation/arithematic.java
@@ -0,0 +1,26 @@
+class arithematic {
+    public static void main (String args[]) {
+
+        int num1 = 7;
+        int num2 = 5;
+        double result = num1 / num2;
+        System.out.println(result); 
+
+        num1 *= 2;
+        System.out.println(num1);
+
+        // int inc = ++num2;
+        // System.out.println(inc);
+
+        int inc1 = num2++;
+        System.out.println(inc1);
+
+
+        //Relational Operators
+         int x = 2;
+         int y = 2;
+         boolean res = x != y;
+         System.out.println(res);
+
+    }
+}
