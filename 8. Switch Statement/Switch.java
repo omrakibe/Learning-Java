ad259f4d322cd192907439ee5e7c9d68148da0f5
Om Rakibe <rakibeom30@gmail.com>
2024-12-21 12:04:02 +0530

    Learned new update in switch using arrow and yield keyword

--- 8. Switch Statement/Switch.java
+++ 8. Switch Statement/Switch.java
@@ -2,7 +2,7 @@ class Switch {
     public static void main (String [] args) {
          
         int i = 9;
-
+        String res = "";
         switch(i) {
             case 1:
                 System.out.println("Monday");
@@ -28,5 +28,43 @@ class Switch {
             default: 
                 System.out.println("Enter num between 1 to 7");
         }
+
+        System.out.println("");
+
+        // Another way
+        res = switch(i) {
+            case 1 : yield "Monday";
+            case 2 : yield "Tuesday";
+            case 3 : yield "wednesday";
+            default : yield "Sunday";
+        };
+        System.out.println(res);
+
+        System.out.println("");
+
+        //update style of Switch Statement
+
+        String day = "Monday" ;
+        String result = "";
+
+        switch(day) {
+            case "Saturday", "Sunday" -> result = "5.30 am";
+            case "Monday" -> result = "6 am";
+            default -> result = "5 am";
+        }
+        System.out.println(result);
+         
+        System.out.println("");
+
+        //Another way 
+        day = "Sunday";
+
+        result = switch(day) {
+            case "Monday" -> "6 am";
+            case "Saturday", "Sunday" -> "5.30am";
+            default -> "5 am";
+        };
+
+        System.out.println(result);
     }
 }
