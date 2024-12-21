bfaf61b0fa0612c72e6f8f31c17c48297df56eb4
Om Rakibe <rakibeom30@gmail.com>
2024-12-21 08:53:34 +0530

    Learned about conditional statement

---
+++ 6. Conditional Statements/condition.java
@@ -0,0 +1,26 @@
+class condition 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("To check the num is even or odd: ");
+        int x = 3; 
+        if (x % 2 == 0) {
+            System.out.println("x is Even Number");
+        } else {
+            System.out.println("x is Odd Number");
+        }
+
+        System.out.println("");
+
+        System.out.println("checking grater num: ");
+        int a = 5;
+        int b = 4;
+        if (a > b) 
+        {
+            System.out.println("a is Greater");
+            System.out.println("Thank you");
+        }
+        else
+            System.out.println("b is greater");
+    }
+}
