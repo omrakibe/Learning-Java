9a6254f70ea93e129454e0c435cf33f643ff1eac
Om Rakibe <rakibeom30@gmail.com>
2024-12-21 09:42:16 +0530

    Learned Ternary Operator

---
+++ 7. Ternary operator/ternary.java
@@ -0,0 +1,28 @@
+class ternary {
+    public static void main (String [] args) {
+
+        int x = 0;
+        int result = 0;
+
+        // if (x % 2 == 0)
+        // {   
+        //     result = x;
+        //     System.out.println("num is even");
+        // }
+        // else {
+        //     result = 20;
+        //     System.out.println("num is odd");
+        // }
+        // System.out.println(result);
+
+        result = x % 2 == 0 ? x : 20;
+        if(result == x) 
+            System.out.println("Num is even");
+        else
+            System.out.println("Num is Odd");
+
+        System.out.println(result);
+
+
+    }
+}
