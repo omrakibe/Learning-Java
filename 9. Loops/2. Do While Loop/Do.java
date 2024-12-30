824a35508136297a12551eed23d4538123ac3dfe
Om Rakibe <rakibeom30@gmail.com>
2024-12-30 10:39:03 +0530

    Learned do while loop

---
+++ 9. Loops/2. Do While Loop/Do.java
@@ -0,0 +1,11 @@
+class Do {
+    public static void main (String [] args) {
+
+        int i = 5;
+        do {
+            System.out.println("Hii " + i);
+            i++;
+        }while(i < 2);
+
+    }
+}
