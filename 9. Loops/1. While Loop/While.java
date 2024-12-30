5c1244c52d8a00729d9cce19090d2dce5ccaabc6
Om Rakibe <rakibeom30@gmail.com>
2024-12-30 10:19:37 +0530

    changes occurs in directory

---
+++ 9. Loops/1. While Loop/While.java
@@ -0,0 +1,18 @@
+class While {
+    public static void main(String [] args) {
+        
+        int i = 0;
+        while (i < 3) {
+            System.out.println("hii " + i);
+
+            int j = 0;
+            while(j<3) {
+                System.out.println("Hello");
+                j++;
+            }
+            i++;
+            System.out.println("");
+        }
+        
+    }
+}
