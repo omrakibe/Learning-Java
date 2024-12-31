3553b10a11aef223b476192b0e6867370706905c
Om Rakibe <rakibeom30@gmail.com>
2024-12-31 12:49:07 +0530

    Learned Jagged array and 3D array

--- 11. Array /2. Jagged Array/jagged.java
+++ 11. Array /2. Jagged Array/jagged.java
@@ -1,6 +1,27 @@
 class jagged {
     public static void main (String [] args) {
 
-        
+        int arr[][] = new int[3][];     //jagged array
+
+        arr[0] = new int [2];
+        arr[1] = new int [1];
+        arr[2] = new int [4];
+
+        for (int i=0; i<arr.length; i++) {
+
+            for (int j=0; j<arr[i].length; j++) {
+                arr[i][j] = (int)(Math.random() * 10);
+            }
+        }
+
+        //Advance Loop
+        for (int n[] : arr) {
+            for (int m : n) {
+                System.out.print(m + " ");
+            }
+            System.out.println();
+        }
+
+        int arr_3d[][][] = new int[2][2][4];        //3D Array
     }
 }
