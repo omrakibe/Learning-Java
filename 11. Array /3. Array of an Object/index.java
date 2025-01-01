bcd53274f3ebf46d631fcc55526b100274c7b136
Om Rakibe <rakibeom30@gmail.com>
2025-01-01 22:47:32 +0530

    added 2 different class and added their value in an array in main class

--- 11. Array /3. Array of an Object/index.java
+++ 11. Array /3. Array of an Object/index.java
@@ -1,15 +1,19 @@
-class Student {
+class Text {
     public void student() {
-        String arr[] = {"Om", "Rakibe", "Hello World", "Pune"};
+        String arr[] = { "Om", "Rakibe", "Hello World", "Pune" };
 
-        for (int i=0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
     }
 
-    // int rollno;
-    // String name;
-    // String subject;
+}
+
+class Student {
+
+    int rollno;
+    String name;
+    String subject;
 
 }
 
@@ -27,12 +31,39 @@ class index {
             System.out.println(arr[i]);
         }
 
-    //     String arr1 [] = {"Om", "Rakibe", "Hello World"};
-    //     for (int i=0  ;i<arr1.length; i++ ) {
-    //         System.out.println(arr1[i]);
-    //     }
+        // String arr1 [] = {"Om", "Rakibe", "Hello World"};
+        // for (int i=0 ;i<arr1.length; i++ ) {
+        // System.out.println(arr1[i]);
+        // }
 
-        Student obj = new Student(); //obj is refernce vairable.
+        Text obj = new Text(); // obj is refernce vairable.
         obj.student();
+
+        Student s1 = new Student();
+        s1.rollno = 65;
+        s1.name = "Om";
+        s1.subject = "Maths";
+
+        Student s2 = new Student();
+        s2.rollno = 71;
+        s2.name = "Sejal";
+        s2.subject = "CG";
+
+        Student s3 = new Student();
+        s3.rollno = 68;
+        s3.name = "Rudra";
+        s3.subject = "OOP";
+
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        for (int i=0; i<students.length; i++) {
+            
+            System.out.println(students[i].name + " - " + students[i].subject );
+            
+        }
+
     }
 }
